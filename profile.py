adb41ceef5f081283bc535e3eaeffa21cff26d86
vini-bgs <viniciustheros@gmail.com>
2026-06-28 08:42:33 -0300

    att: adicionando logs no arquivo profile.py

--- profile.py
+++ profile.py
@@ -1,5 +1,7 @@
 from extract import csvs_for_df
 from pathlib import Path
+from tqdm import tqdm
+from loguru import logger
 import pandas as pd
 
 
@@ -9,29 +11,35 @@ def profile_columns(df: pd.DataFrame) -> dict:
     coluna para ser utilizado no Schema da tabela
     """
     schema = dict()
-    df = df.head(1000)
+    logger.info("⏳ Verificando TOP 1000 linhas do DataFrame...")
+    try:
+        df = df.head(1000)
 
-    for col in df.columns:
-        sample = df[col].dropna()
+        for col in tqdm(df.columns):
+            sample = df[col].dropna()
 
-        if len(sample) == 0:
+            if len(sample) == 0:
+                schema[col] = "str"
+                continue
+
+            converted = pd.to_datetime(sample, errors="coerce", format="%Y-%m-%d")
+            if converted.notna().sum() / len(sample) > 0.8:
+                schema[col] = "date"
+                continue
+
+            converted = pd.to_numeric(sample, errors="coerce")
+            if converted.notna().sum() / len(sample) > 0.8:
+                if (converted.dropna() % 1 == 0).all():
+                    schema[col] = "int"
+                else:
+                    schema[col] = "float"
+                continue
+
             schema[col] = "str"
-            continue
+        logger.info("✅ Schema da tabela criado")
 
-        converted = pd.to_datetime(sample, errors="coerce", format="%Y-%m-%d")
-        if converted.notna().sum() / len(sample) > 0.8:
-            schema[col] = "date"
-            continue
-
-        converted = pd.to_numeric(sample, errors="coerce")
-        if converted.notna().sum() / len(sample) > 0.8:
-            if (converted.dropna() % 1 == 0).all():
-                schema[col] = "int"
-            else:
-                schema[col] = "float"
-            continue
-
-        schema[col] = "str"
+    except TypeError as err:
+        logger.error(f"🛑 {err}. Erro no DataFrame.")
 
     return schema
 
